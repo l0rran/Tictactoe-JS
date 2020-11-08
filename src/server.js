852a8bce0cbf26b1a5ccea016c4e1b622029a174
Wesley <wesleycct@gmail.com>
2020-11-08 17:30:39 -0300

    update rooms functionality

--- src/server.js
+++ src/server.js
@@ -12,42 +12,138 @@ app.use(express.static("public"));
 
 let rooms = {};
 
-function joinRoom(name, socket) {
-  if (rooms[name]) {
+function formatRoomsForPlayers() {
+  let roomsForPlayers = [];
+  for (let [key, value] of Object.entries(rooms)) {
+    roomsForPlayers = [
+      ...roomsForPlayers,
+      { name: key, playersAmount: value.players.length },
+    ];
+  }
+  return roomsForPlayers;
+}
+
+function updateRooms(data) {
+  if (data && data.socket) {
+    data.socket.emit("update rooms", { rooms: formatRoomsForPlayers() });
+  } else {
+    sockets
+      .to("lobby")
+      .emit("update rooms", { rooms: formatRoomsForPlayers() });
+  }
+}
+
+const acceptedJoins = {
+  lobby: ({ socket }) => {
+    if (socket.room) {
+      leave({ type: "room", data: { socket } });
+    }
+    if (!socket.inLobby) {
+      socket.join("lobby");
+      socket.emit("joined lobby");
+      updateRooms({ socket, rooms });
+      socket.inLobby = true;
+    }
+  },
+  room: ({ name, socket }) => {
+    if (!rooms[name] || rooms[name].players.length >= 2) {
+      //TODO: Display error message
+      return;
+    }
     rooms[name].players.push({ playerId: socket.id });
     socket.room = rooms[name];
-    socket.room.game.eventEmitter.on("changeBoardValue", (data) => {
-      socket.emit("boardPositionChanged", data);
+    socket.room.game.events.on("board value changed", (data) => {
+      let newData = { ...data };
+      if (newData.value.playerId) {
+        if (newData.value.playerId === socket.id) {
+          newData.value = "player";
+        } else {
+          newData.value = "opponent";
+        }
+      }
+
+      socket.emit("board value changed", newData);
     });
-    socket.leave("lobby");
+
+    socket.room.game.events.on("game state changed", (data) => {
+      let newData = { ...data };
+      if (newData.winner) {
+        if (newData.winner.playerId === socket.id) {
+          newData.winner = "player";
+        } else {
+          newData.winner = "opponent";
+        }
+      }
+
+      socket.emit("game state changed", newData);
+    });
+
+    socket.room.game.events.on("turn changed", ({ turnPlayer }) => {
+      if (turnPlayer) {
+        socket.emit("turn changed", {
+          turnPlayer: turnPlayer.playerId === socket.id ? "player" : "opponent",
+        });
+      }
+    });
+
+    leave({ type: "lobby", data: { socket } });
     socket.join(name);
-    socket.emit("joined room", name);
+    socket.emit("joined room", { name });
+    socket.room.game.events.emit("player joined", { playerId: socket.id });
+    updateRooms(rooms);
     console.log(`Room players:`);
     console.table(socket.room.players);
-  }
-}
+  },
+};
 
-function leaveRoom(socket) {
-  if (socket.room) {
-    socket.room.players = socket.room.players.filter((player) => {
-      return player.playerId !== socket.id;
-    });
-
-    socket.leave(socket.room.name);
-    socket.room = undefined;
-    socket.join("lobby");
-    socket.emit("left room");
+const join = ({ type, data }) => {
+  if (type) {
+    acceptedJoins[type](data);
   }
-}
+};
+
+const acceptedLeaves = {
+  room: ({ socket }) => {
+    if (socket.room) {
+      socket.room.players = socket.room.players.filter((player) => {
+        return player.playerId !== socket.id;
+      });
+      if (Object.keys(socket.room.players).length === 0) {
+        delete rooms[socket.room.name];
+        updateRooms({ rooms });
+      }
+      socket.room.game.events.emit("player left", { playerId: socket.id });
+      socket.leave(socket.room.name);
+      delete socket.room;
+      socket.emit("left room");
+      join({ type: "lobby", data: { socket } });
+      updateRooms();
+    }
+  },
+  lobby: ({ socket }) => {
+    if (socket.inLobby) {
+      socket.leave("lobby");
+      socket.emit("left lobby");
+      socket.inLobby = false;
+    }
+  },
+};
+
+const leave = ({ type, data }) => {
+  if (type) {
+    acceptedLeaves[type](data);
+  }
+};
 
 sockets.on("connection", (socket) => {
   const playerId = socket.id;
-  socket.join("lobby");
   console.log(`Player connected on Server with id: ${playerId}`);
-  const roomsNames = Object.keys(rooms);
   socket.emit("gameInfo", {
     constants: { pieces: Tictactoe.pieces, states: Tictactoe.states },
-    rooms: roomsNames,
+  });
+
+  socket.on("disconnect", () => {
+    leave({ type: "room", data: { socket } });
   });
 
   socket.on("play", (data) => {
@@ -55,7 +151,6 @@ sockets.on("connection", (socket) => {
       console.log(
         `Player: ${playerId} in room: ${socket.room.name} trying to play in position: ${data.position}`
       );
-
       socket.room.game.play({ playerId, position: data.position });
     }
   });
@@ -76,19 +171,17 @@ sockets.on("connection", (socket) => {
         game: new Tictactoe(),
         players: [],
       };
-      sockets.to("lobby").emit("update rooms", { rooms: Object.keys(rooms) });
-      joinRoom(name, socket);
+      updateRooms({ rooms });
+      join({ type: "room", data: { name, socket } });
     }
   });
 
-  socket.on("join room", ({ name }) => {
-    if (rooms[name].players.length < 2) {
-      joinRoom(name, socket);
-    }
+  socket.on("join", ({ type, data }) => {
+    join({ type, data: { socket, ...data } });
   });
 
-  socket.on("leave room", () => {
-    leaveRoom(socket);
+  socket.on("leave", ({ type, data }) => {
+    leave({ type, data: { socket, ...data } });
   });
 });
 
--- src/tictactoe.js
+++ src/tictactoe.js
@@ -4,9 +4,7 @@ class TictactoeEmitter extends EventEmitter {}
 
 class Tictactoe {
   static pieces = {
-    EMPTY: "",
-    X: "X",
-    O: "O",
+    EMPTY: "empty",
   };
 
   static states = {
@@ -14,12 +12,9 @@ class Tictactoe {
     Over: "over",
   };
 
-  players = {
-    0: {  id: 0, playerId: undefined },
-    1: {  id: 1, playerId: undefined },
-  };
+  players = {};
 
-  eventEmitter = new TictactoeEmitter();
+  events = new TictactoeEmitter();
 
   #board;
   #turnPlayer;
@@ -38,10 +33,31 @@ class Tictactoe {
   constructor(players) {
     this.#board = Array(9).fill(Tictactoe.pieces.EMPTY);
     this.restart();
-    this.eventEmitter.on("changeTurn", this.verifyWin);
     if (players) {
       this.players = players;
     }
+
+    this.events.on("player joined", ({ playerId }) => {
+      let addId;
+      if (!this.players[0]) {
+        addId = 0;
+      } else if (!this.players[1]) {
+        addId = 1;
+      }
+      if (addId !== undefined) {
+        this.players[addId] = { id: addId, playerId };
+      }
+    });
+
+    this.events.on("player left", ({ playerId }) => {
+      for (let [key, player] of Object.entries(this.players)) {
+        if (player.playerId === playerId) {
+          delete this.players[key];
+        }
+      }
+    });
+
+    this.changeGameState(Tictactoe.states.Over);
   }
 
   getBoard() {
@@ -50,33 +66,40 @@ class Tictactoe {
 
   changeTurn(playerIndex) {
     this.#turnPlayer = this.players[playerIndex];
-    this.eventEmitter.emit("changeTurn", {
+    this.events.emit("turn changed", {
       turnPlayer: this.players[playerIndex],
     });
+    this.verifyWin();
   }
 
   changeBoardValue(position, value) {
     this.#board[position] = value;
-    this.eventEmitter.emit("changeBoardValue", { position, value });
+    this.events.emit("board value changed", { position, value });
   }
 
   changeGameState(data) {
     if (data.state === this.#gameState) return;
     this.#gameState = data.state;
-    console.log("New gameState:");
-    console.table(data);
-    this.eventEmitter.emit("changeGameState", { state: data.state });
+    this.events.emit("game state changed", data);
   }
 
   restart() {
-    this.#board.forEach((value, index) => {
-      this.changeBoardValue(index, Tictactoe.pieces.EMPTY);
-    });
-    this.changeGameState({ state: Tictactoe.states.Playing });
-    this.changeTurn(0);
+    if (
+      this.#gameState !== Tictactoe.states.Playing &&
+      this.players[0] &&
+      this.players[1]
+    ) {
+      this.#board.forEach((value, index) => {
+        this.changeBoardValue(index, Tictactoe.pieces.EMPTY);
+      });
+      this.changeGameState({ state: Tictactoe.states.Playing });
+      this.changeTurn(0);
+    }
   }
+
   verifyWin() {
-    if (this.gameState !== Tictactoe.states.Playing) return;
+    if (this.#gameState !== Tictactoe.states.Playing) return;
+
     let emptyPositions = 0;
     this.#winPositions.forEach((positions) => {
       let value = Tictactoe.pieces.EMPTY;
@@ -84,15 +107,15 @@ class Tictactoe {
       positions.forEach((position) => {
         if (this.#board[position] === Tictactoe.pieces.EMPTY) {
           emptyPositions++;
-        } else if (board[position] !== value) {
-          value = board[position];
+        } else if (this.#board[position] !== value) {
+          value = this.#board[position];
           same = 1;
         } else {
           same++;
         }
       });
-      if (value !== pieces.EMPTY && same === 3) {
-        changeGameState({
+      if (value !== Tictactoe.pieces.EMPTY && same === 3) {
+        this.changeGameState({
           state: Tictactoe.states.Over,
           winner: value,
           winPositions: positions,
@@ -101,15 +124,22 @@ class Tictactoe {
       }
     });
     if (emptyPositions === 0) {
-      changeGameState({ state: Tictactoe.states.Over });
+      this.changeGameState({ state: Tictactoe.states.Over });
     }
   }
   play({ playerId, position }) {
-    if (this.#gameState !== Tictactoe.states.Playing) return;
-    if (this.#turnPlayer.playerId !== playerId) return;
+    if (this.#gameState !== Tictactoe.states.Playing) {
+      return;
+    }
+    if (this.#turnPlayer.playerId !== playerId) {
+      console.log("Not player turn!");
+      console.log(`Player turn id: ${this.#turnPlayer.playerId}`);
+      console.log(`Player Id: ${playerId}`);
+      return;
+    }
     position = Number(position);
 
-    if (position < 0 || position > 9) {
+    if (isNaN(position) || position < 0 || position > 9) {
       console.error("Tictactoe: Invalid position");
       return;
     }
@@ -119,6 +149,7 @@ class Tictactoe {
     }
     this.changeBoardValue(position, this.#turnPlayer);
     this.changeTurn(this.#turnPlayer == this.players[0] ? 1 : 0);
+    console.table(this.#board);
   }
 
   hasValue(position) {
@@ -127,149 +158,3 @@ class Tictactoe {
 }
 
 export default Tictactoe;
-
-/*
-class Observable {
-  constructor() {
-    this.observers = [];
-  }
-
-  subscribe(f) {
-    this.observers.push(f);
-  }
-
-  unsubscribe(f) {
-    this.observers = this.observers.filter((subscriber) => subscriber !== f);
-  }
-
-  notify(data) {
-    this.observers.forEach((observer) => observer(data));
-  }
-}
-
-const Tictactoe = (() => {
-  const pieces = {
-    EMPTY: "",
-    X: "X",
-    O: "O",
-  };
-
-  const states = {
-    Playing: "playing",
-    Over: "over",
-  };
-
-  const onPlayerTurnChange = new Observable();
-  const onBoardValueChange = new Observable();
-  const onGameStateChange = new Observable();
-
-  const board = Array(9).fill(pieces.EMPTY);
-  let turnPlayer = pieces.X;
-  let gameState = states.Over;
-
-  function changeTurn(newPlayer) {
-    turnPlayer = newPlayer;
-    onPlayerTurnChange.notify({
-      turnPlayer: turnPlayer,
-    });
-  }
-
-  function changeBoardValue(position, value) {
-    board[position] = value;
-    onBoardValueChange.notify({
-      position: position,
-      value: value,
-    });
-  }
-
-  function changeGameState(data) {
-    if (data.state === gameState) return;
-    gameState = data.state;
-    onGameStateChange.notify(data);
-    console.log("New gameState:");
-    console.table(data);
-  }
-
-  function reset() {
-    board.forEach((value, index) => {
-      changeBoardValue(index, pieces.EMPTY);
-    });
-    changeGameState({ state: states.Playing });
-    changeTurn(pieces.X);
-  }
-
-  const winPositions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  function verifyWin() {
-    if (gameState !== states.Playing) return;
-    let emptyPositions = 0;
-    winPositions.forEach((positions) => {
-      let value = pieces.EMPTY;
-      let same = 0;
-      positions.forEach((position) => {
-        if (board[position] === pieces.EMPTY) {
-          emptyPositions++;
-        } else if (board[position] !== value) {
-          value = board[position];
-          same = 1;
-        } else {
-          same++;
-        }
-      });
-      if (value !== pieces.EMPTY && same === 3) {
-        changeGameState({
-          state: states.Over,
-          winner: value,
-          winPositions: positions,
-        });
-        return;
-      }
-    });
-    if (emptyPositions === 0) {
-      changeGameState({ state: states.Over });
-    }
-  }
-
-  onPlayerTurnChange.subscribe(verifyWin);
-
-  reset();
-
-  return {
-    gameInfo: { constants: { pieces: pieces, states: states } },
-    onPlayerTurnChange: onPlayerTurnChange,
-    onBoardValueChange: onBoardValueChange,
-    onGameStateChange: onGameStateChange,
-    play: function (position) {
-      if (gameState !== states.Playing) return;
-
-      position = Number(position);
-
-      if (position < 0 || position > 9) {
-        console.error("Tictactoe: Invalid position");
-        return;
-      }
-      if (this.hasValue(position)) {
-        console.error("Tictactoe: Position already has a value");
-        return;
-      }
-      changeBoardValue(position, turnPlayer);
-      changeTurn(turnPlayer == pieces.X ? pieces.O : pieces.X);
-    },
-    hasValue: function (position) {
-      return board[position] !== pieces.EMPTY;
-    },
-    restart: function () {
-      reset();
-    },
-  };
-})();
-*/
